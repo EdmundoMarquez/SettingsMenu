28279e32b00927472d0bde22364744ea440d13ee
Edmundo Márquez <edmundomarquez@protonmail.com>
2022-03-19 01:42:58 -0600

    feat[Fonts]: Implemented accessibility text and configurations

---
+++ SettingsMenu/Assets/Scripts/Fonts/AccessibleFontPresets.cs
@@ -0,0 +1,36 @@
+﻿using TMPro;
+
+
+namespace SettingsMenu
+{
+    using UnityEngine;
+    
+    [CreateAssetMenu(fileName = "AccesibleFontPresets", menuName = "Accessibility/FontPresets", order = 0)]
+    public class AccessibleFontPresets : ScriptableObject 
+    {
+        public int FixedPreset = 1;
+        public float FixedSize = 35;
+        [Header("Font Presets")]
+        public TMP_FontAsset SerifFont;
+        public TMP_FontAsset SansSerifFont;
+        public TMP_FontAsset OpenDyslexicFont;
+        [Header("Font Sizes")]
+        [Range(5,20)] public float smallSize = 20;
+        [Range(20,35)] public float mediumSize = 35;
+        [Range(35,50)] public float largeSize = 50;
+
+        public TMP_FontAsset GetFont()
+        {
+            switch(FixedPreset)
+            {
+                default:
+                case 0:
+                    return SerifFont;
+                case 1:
+                    return SansSerifFont;
+                case 2:
+                    return OpenDyslexicFont;
+            }
+        }
+    }       
+}
---
+++ SettingsMenu/Assets/Scripts/Fonts/UIAccesibleText.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using SettingsMenu.EventBus;
+using TMPro;
+
+namespace SettingsMenu
+{
+    public class UIAccesibleText : MonoBehaviour, IEventReceiver<FontChanged>
+    {
+        [SerializeField] private AccessibleFontPresets _fontPresets;
+        [SerializeField] private TMP_Text _text;
+
+        private void Start() 
+        {
+            _text.font = _fontPresets.GetFont();
+        }
+
+        public void OnEvent(FontChanged e)
+        {
+            _text.font = _fontPresets.GetFont();
+        }
+
+        private void OnEnable() 
+        {
+            EventBus<FontChanged>.Register(this);    
+        }
+
+        private void OnDisable() 
+        {
+            EventBus<FontChanged>.UnRegister(this);    
+        }
+    }    
+}
