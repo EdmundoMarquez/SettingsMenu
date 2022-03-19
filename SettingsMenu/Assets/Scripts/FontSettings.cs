ff229ca502186ff25d34f7583fe2fb9016eab425
Edmundo Márquez <edmundomarquez@protonmail.com>
2022-03-19 01:47:33 -0600

    feat[Font]: Font change and dropdown initialization with settings

---
+++ SettingsMenu/Assets/Scripts/FontSettings.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+using SettingsMenu.EventBus;
+
+namespace SettingsMenu
+{
+    public class FontSettings : MonoBehaviour
+    {
+        [SerializeField] private AccessibleFontPresets _fontPresets = null;
+        [SerializeField] private CustomDropdown _fontTypeDropdown = null;
+
+        private void Start() 
+        {
+            SetFontPresetDropdown();
+        }
+
+        private void SetFontPresetDropdown()
+        {
+            List<string> options = new List<string>();
+            options.Add("Serif");
+            options.Add("Sans Serif");
+            options.Add("Open Dyslexic");
+            
+            _fontTypeDropdown.Init(options, _fontPresets.FixedPreset); 
+        }
+
+        public void SetFontPreset(int presetIndex)
+        {
+            _fontPresets.FixedPreset = presetIndex;
+            EventBus<FontChanged>.Raise(new FontChanged{});
+        }
+    }
+}
