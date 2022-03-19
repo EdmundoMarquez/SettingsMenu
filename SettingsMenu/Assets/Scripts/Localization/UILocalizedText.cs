129a288f86269e65bc5a916f0dc9709e94749d07
Edmundo Márquez <edmundomarquez@protonmail.com>
2022-03-19 02:03:20 -0600

    feat[Localization]: Implemented localized text class in dropdowns, sliders and checkers

---
+++ SettingsMenu/Assets/Scripts/Localization/UILocalizedText.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using TMPro;
+using SettingsMenu.EventBus;
+
+namespace SettingsMenu.Language
+{
+    public class UILocalizedText : MonoBehaviour, IEventReceiver<LanguageChanged>
+    {
+        [SerializeField] private string _textId;
+        [SerializeField] private TextMeshProUGUI _text;
+
+        public void Refresh() 
+        {
+            string text = LanguageTextConverter.LoadTitleText(_textId);
+            _text.text = text; 
+            Debug.Log(text);
+        }
+
+        public void OnEvent(LanguageChanged e)
+        {
+            Refresh();
+        }
+
+        private void OnEnable() 
+        {
+            EventBus<LanguageChanged>.Register(this);    
+        }
+
+        private void OnDisable() 
+        {
+            EventBus<LanguageChanged>.UnRegister(this);    
+        }
+    }    
+}
