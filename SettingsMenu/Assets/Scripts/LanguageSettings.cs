704f3728aade432683185024ec2ff725f554a775
Edmundo Márquez <edmundomarquez@protonmail.com>
2022-03-19 02:08:37 -0600

    feat[Settings]: Implemented language change and dropdown initialization

---
+++ SettingsMenu/Assets/Scripts/LanguageSettings.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using TMPro;
+using SettingsMenu.Language;
+using System.Collections.Generic;
+
+namespace SettingsMenu
+{
+    public class LanguageSettings : MonoBehaviour
+    {
+        [SerializeField] private TextAssetData[] _supportedLanguages = null;
+        [SerializeField] private LanguageTextConverter _languageConverter = null;
+        [SerializeField] private CustomDropdown _languageDropdown = null;
+
+        private void Start() 
+        {
+            SetLanguagesDropdown();
+        }
+
+        private void SetLanguagesDropdown()
+        {
+            List<string> options = new List<string>();
+
+            foreach(var language in _supportedLanguages)
+            {
+                options.Add(language.name);
+            }
+            
+            _languageDropdown.Init(options, 0);
+        }
+
+        public void ChangeLanguage(int languageIndex)
+        {
+            string id = _supportedLanguages[languageIndex].LanguageId;
+            _languageConverter.ChangeLanguage(id);
+        }
+    }    
+}
