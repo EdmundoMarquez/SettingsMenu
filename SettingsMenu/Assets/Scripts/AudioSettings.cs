c5dec78edcef0d3037f6cd2417a9930d42926f5d
Edmundo Márquez <edmundomarquez@protonmail.com>
2022-03-19 21:59:01 -0600

    feat[Settings]: Change mixer volumes with values from settings sliders

---
+++ SettingsMenu/Assets/Scripts/AudioSettings.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using UnityEngine.Audio;
+
+namespace SettingsMenu
+{
+    public class AudioSettings : MonoBehaviour
+    {
+        [SerializeField] private AudioMixer _audioMixer = null;
+
+        public void SetMasterVol(float volume)
+        {
+            _audioMixer.SetFloat("master_vol", volume);
+            Debug.Log("Changed value");
+        }
+
+        public void SetMusicVol(float volume)
+        {
+            _audioMixer.SetFloat("music_vol", volume);
+        }
+
+        public void SetSfxVol(float volume)
+        {
+            _audioMixer.SetFloat("sfx_vol", volume);
+        }
+
+        public void SetAmbientVol(float volume)
+        {
+            _audioMixer.SetFloat("ambient_vol", volume);
+        }
+    }    
+}
